38688165280d59699cf63c9c2fd3e2cdd1b57a2c
三浦真 <>
2020-03-26 20:27:46 +0900

    スペルミス修正

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ if (isset($_REQUEST[('res')])) {
         <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]</p>
         <p class="day"><a href="view.php?id=<?php print(htmlspecialchars($post['id'])); ?>"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
 
-        <?php if ($post['reply_messame_id'] > 0): ?>
+        <?php if ($post['reply_message_id'] > 0): ?>
           <a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id'], ENT_QUOTES)); ?>">
         返信元のメッセージ</a>
         <?php endif; ?>
